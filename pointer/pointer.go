253c64e2b4ab70fb0b19c0603aa9e2221da3f543
Prakhar-Shankar <prakharshankar247@gmail.com>
2025-07-27 00:53:02 +0530

    Pointers

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -1,5 +1,10 @@
 package pointer
 
+import (
+	"fmt"
+	"errors"
+)
+
 
 
 type Bitcoin int //go lrts you create new type from the existing one
@@ -14,6 +19,24 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() Bitcon {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+type Stringer interface{
+	String() string
+}
+
+func (b Bitcoin) String() string{
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error{
+
+	if amount > w.balance{
+		return errors.New("oh no")
+	}
+
+	w.balance -= amount
+	return nil 
 }
